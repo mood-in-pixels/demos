36472c2a963e6f0e55a22c849028e7f0746a1262
Gintas <maziukas2008@gmail.com>
2017-12-17 21:14:45 -0600

    updated members.js for automated loading of content

--- App/public/js/members.js
+++ App/public/js/members.js
@@ -18,41 +18,73 @@ var user_id;
         // startSlide(event);
       });
 
-    // Setup Global Variables 
+    // Setup Moods for Carousel Function
     // -------------------------------------------------------------------------------------
+    $.get("/api/mood_data").then(function(data) {
+      for (var i = 0; i < data.length; i++) {
 
-    // Setup Swipe / Carousel Function 
-    // -------------------------------------------------------------------------------------
-    $('.autoplay').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        swipe: true,
-        swipeToSlide: true,
-        dots: false,
-        arrows: false
+
+        var newItem = $("<div>")
+          newItem.addClass("item")
+
+        var newButton = $("<button>")
+          newButton.addClass("pixel");
+          newButton.css({"background": "radial-gradient("+ data[i].color_one +", " + data[i].color_two+")"});
+          // newButton.attr("role","submit");
+          // newButton.attr("type","submit");
+          newButton.attr("data_mood_id",data[i].id);
+          newButton.attr("data-mood",data[i].mood_description);
+          newButton.attr("data-color",data[i].color_one);
+          // newButton.attr("id","blue");
+
+        var newSpan = $("<span>");
+          newSpan.addClass("valence");
+          newSpan.text(data[i].mood_description);
+
+          newButton.append(newSpan);
+          newItem.append(newButton);
+        $(".carousel").append(newItem)
+
+      }
+      autoplay();
     });
+
+
+        // Setup Swipe / Carousel Function
+        // -------------------------------------------------------------------------------------
+
+      function autoplay() {
+        $('.autoplay').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            swipe: true,
+            swipeToSlide: true,
+            dots: false,
+            arrows: false
+        });
+
 
     // Mouse events *hover (to reveal mood/valence description to users); *click to log mood
     // -------------------------------------------------------------------------------------
 
-   
-    $('.pixel').hover(function() {
-        $(this).find('.valence').fadeIn(300);
-    },
-    function() {
-        $(this).find('.valence').fadeOut(1000);
-    });
-    
 
-    $(".pixel").on("click", function() {
+    $(document).on("mouseover",".pixel",function(){
+        $(this).find('.valence').fadeIn(300);
+        $(this).find('.valence').fadeOut(1000);
+      });
+
+
+
+
+    $(document).on("click",".pixel",function(){
          // event.preventDefault();
-        var date = new Date();
+        var date = moment().format("MM/DD/YYYY");
         var mood_id = $(this).attr("data-mood");
         var color = $(this).attr("data-color");
         var DimMoodId = $(this).attr("data_mood_id")
- 
+
         var userData = {
           mood_id: mood_id,
           color: color,
@@ -60,32 +92,160 @@ var user_id;
           DimMoodId: DimMoodId
         };
 
-        
-        // ^^^^^^^ ADD MOMENT.JS TO RESTRICT USERS TO DAILY USE !!!!
-        // __________________________________________________________
+
+                // If we have an username and password we run the loginUser function and clear the form
+                logDailyMood(user_id, userData.mood_id, userData.color, userData.mood_date, userData.DimMoodId);
+
+              });
+
+              function logDailyMood(user_id, mood_id, color, date, DimMoodId) {
+                $.post("/api/dailymoods", {
+                  user_id: user_id,
+                  mood_id: mood_id,
+                  color: color,
+                  mood_date: date,
+                  DimMoodId: DimMoodId
+
+                }).then(function(data) {
+                  window.location.replace(data);
+                  // If there's an error, log the error
+                }).catch(function(err) {
+                  console.log(err);
+                });
+              }
 
 
-        // If we have an username and password we run the loginUser function and clear the form
-        logDailyMood(user_id, userData.mood_id, userData.color, userData.mood_date, userData.DimMoodId);
-        
-      });
+              ////////////////////////////////////////////////////////////////////////////////////
+              //////////////         Adding shading on hover for calendar   //////////////////////////
 
-      // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-      function logDailyMood(user_id, mood_id, color, date, DimMoodId) {
-        $.post("/api/dailymoods", {
-          user_id: user_id,
-          mood_id: mood_id,
-          color: color,
-          mood_date: date,
-          DimMoodId: DimMoodId
 
-        }).then(function(data) {
-          window.location.replace(data);
-          // If there's an error, log the error
-        }).catch(function(err) {
-          console.log(err);
+               $(function(){
+                 $('li[class^="cat-"]').mouseover(function(){
+                    var currentClass = $(this).attr('class').split(' ')[0];
+                    if(currentClass != 'empty'){
+                      $('.main > li').addClass('deactivate');
+                      $('.' + currentClass).removeClass('deactivate');
+                    }
+                  });
+
+
+                  $('li[class^="type-"]').mouseover(function(){
+                    var currentClass = $(this).attr('class').split(' ')[0];
+                    if(currentClass != 'empty'){
+                      $('.main > li').addClass('deactivate');
+                      $('.' + currentClass).removeClass('deactivate');
+                    }
+                  });
+
+
+                  $('.main > li').mouseout(function(){
+                    var currentClass = $(this).attr('class').split(' ')[0];
+                     $('.main > li').removeClass('deactivate');
+                  });
+
+              });
+
+
+
+
+                ////////////////////////////////////////////////////////////////////////////////////
+                //////////////         Dynamicaly creating calendar table   ////////////////////////
+
+
+
+
+
+               function calendar(year) {
+               console.log("creating calendar")
+                 for (var monthCount = 0; monthCount < 13; monthCount++) {
+                 var newList = $("<ul>");
+                 newList.addClass("main");
+                   for (var dayCount = 0; dayCount < 32; dayCount++) {
+
+                     if (dayCount===0 && monthCount===0) {
+                       var newListItem = $("<li>")
+                       newListItem.addClass("cEmpty")
+                       // newListItem.addClass("cLabel");
+                       newList.append(newListItem)
+                     }
+                     else if (monthCount ===0)
+                     {
+                       var newListItem = $("<li>");
+                       newListItem.addClass("cEmpty");
+                       var labelDiv = $("<div>")
+                       labelDiv.addClass("cLabel");
+                       labelDiv.text(dayCount);
+                       newListItem.append(labelDiv);
+                       newList.append(newListItem);
+
+                     } else if (dayCount ===0) {
+                       var monthName = moment(monthCount, "M").format("MMM")
+                       var newListItem = $("<li>");
+                       newListItem.addClass("cEmpty");
+                       var labelDiv = $("<div>");
+                       labelDiv.addClass("cLabel");
+                       labelDiv.text(monthName.substring(0,1));
+                       newListItem.append(labelDiv);
+                       newList.append(newListItem)
+                     } else {
+
+                     var newListItem = $("<li>")
+
+                       var dateDiv = $("<div>")
+                       var date = String(monthCount) + "/" + String(dayCount) + "/" + year
+                       var dateFormat = "M/D/YYYY"
+                       var convertedDate = moment(date, dateFormat)
+                       var dayOfTheWeek = convertedDate.format("d") // sunday =0
+
+
+
+                       if (!convertedDate.isValid()) {
+                         newListItem.addClass("cEmpty")
+                       } else {
+                       dateDiv.addClass("cDate")
+                       dateDiv.addClass(date)
+                       dateDiv.text(convertedDate.format("MM/DD/YY"));
+                       newListItem.append(dateDiv)
+
+                       var location = $("<div>")
+                       location.addClass("cLocation")
+                       location.text("Gary, IN")
+                       newListItem.append(location)
+
+                       var mood = Math.floor(Math.random() * (9 - 1)) + 1
+                       newListItem.addClass("type-" + mood)
+                       newListItem.addClass("cat-" + dayOfTheWeek)
+                       }
+
+
+
+                     newList.append(newListItem)
+                   }
+                   }
+                   $(".wrapper").append(newList)
+                 }
+               }
+
+               calendar(2017);
+
+
+
+
+               $( window ).on( "orientationchange", function( event ) {
+
+                 if(window.innerHeight > window.innerWidth) {
+                  $(".row").addClass("hidden")
+                  $(".carousel").addClass("hidden")
+                  $(".wrapper").removeClass("hidden")
+                 } else {
+                  $(".wrapper").addClass("hidden")
+                  $(".row").removeClass("hidden")
+                  $(".carousel").removeClass("hidden")
+                 }
+               });
+
+
+
+
+
         });
-      }// do no delete (startSlide)
-
-
-}); // *** END ***
