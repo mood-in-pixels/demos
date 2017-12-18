eeb745996519a7b9fcd01ca91f19b048ebb390cc
Gintas <maziukas2008@gmail.com>
2017-12-17 21:32:22 -0600

    added one extra column to dim_moods table
    seeds in file

--- App/schema.sql
+++ App/schema.sql
@@ -1,15 +1,32 @@
-DROP DATABASE IF EXISTS 
+DROP DATABASE IF EXISTS
 pixel_moods_db;
 
 CREATE DATABASE pixel_moods_db;
 USE pixel_moods_db;
 
 
--- INSERT INTO dim_moods (mood_description,color,active,date,createdAt,updatedAt,UserID) VALUES ("Happy", "#4caf50",true,current_date(),current_date(),current_date(),1);
--- INSERT INTO dim_moods (mood_description,color,active,date,createdAt,updatedAt,UserID) VALUES ("Stressed", "#ffeb3b",true,current_date(),current_date(),current_date(),1);
--- INSERT INTO dim_moods (mood_description,color,active,date,createdAt,updatedAt,UserID) VALUES ("Angry","#B71C1C",true,current_date(),current_date(),current_date(),1);
--- INSERT INTO dim_moods (mood_description,color,active,date,createdAt,updatedAt,UserID) VALUES ("Content","#00bcd4",true,current_date(),current_date(),current_date(),1);
--- INSERT INTO dim_moods (mood_description,color,active,date,createdAt,updatedAt,UserID) VALUES ("Depressed","#17558C",true,current_date(),current_date(),current_date(),1);
--- INSERT INTO dim_moods (mood_description,color,active,date,createdAt,updatedAt,UserID) VALUES ("Energetic","#ff9800",true,current_date(),current_date(),current_date(),1);
--- INSERT INTO dim_moods (mood_description,color,active,date,createdAt,updatedAt,UserID) VALUES ("Guilty","#673ab7",true,current_date(),current_date(),current_date(),1);
--- INSERT INTO dim_moods (mood_description,color,active,date,createdAt,updatedAt,UserID) VALUES ("Compassionate","#EC407A",true,current_date(),current_date(),current_date(),1);
+
+INSERT INTO dim_moods (id, mood_description,color_one,color_two,active,date,createdAt,updatedAt,UserID)
+VALUES (1,"Happy","#4CAF50","#BEF0CB",true,current_date(),current_date(),current_date(),1);
+
+INSERT INTO dim_moods (id, mood_description,color_one,color_two,active,date,createdAt,updatedAt,UserID)
+VALUES (2,"Stressed","#ffe801","#fcf7bc",true,current_date(),current_date(),current_date(),1);
+
+INSERT INTO dim_moods (id, mood_description,color_one,color_two,active,date,createdAt,updatedAt,UserID)
+VALUES (3,"Angry","#d33a32","#FFB9C2",true,current_date(),current_date(),current_date(),1);
+
+INSERT INTO dim_moods (id, mood_description,color_one,color_two,active,date,createdAt,updatedAt,UserID)
+VALUES (4,"Content","#00BCD4","#BBE5FB",true,current_date(),current_date(),current_date(),1);
+
+INSERT INTO dim_moods (id, mood_description,color_one,color_two,active,date,createdAt,updatedAt,UserID)
+VALUES (5,"Depressed","#2196F3","#88FAFF",true,current_date(),current_date(),current_date(),1);
+
+INSERT INTO dim_moods (id, mood_description,color_one,color_two,active,date,createdAt,updatedAt,UserID)
+VALUES (6,"Energetic","#FF5722","#FFCC82",true,current_date(),current_date(),current_date(),1);
+
+INSERT INTO dim_moods (id, mood_description,color_one,color_two,active,date,createdAt,updatedAt,UserID)
+VALUES (7,"Guilty","#673AB7","#d6acfa",true,current_date(),current_date(),current_date(),1);
+
+INSERT INTO dim_moods (id, mood_description,color_one,color_two,active,date,createdAt,updatedAt,UserID)
+VALUES (8,"Compassionate","#b3317d","#F5BAF1",true,current_date(),current_date(),current_date(),1);
+
